54e801f62c5ba291037d9e569e9a323bb33a5e6d
Elixa-Mae Pesa <eapesa@gmail.com>
2013-09-09 17:37:13 +0800

    now with basic interface.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var nconf = require("nconf");
 var xmpp = require("node-xmpp");
 var request = require("request");
+var nconf = require("nconf");
+var cons = require("consolidate");
 var express = require("express");
 
 var app = express();
@@ -8,30 +9,6 @@ var app = express();
 nconf.use("file", {
     file: "./config.json"
 });
-
-/*
-conn.on("online", function() {
-    console.log("We're online!");
-    // Set client's presence
-    conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
-    conn.send(new xmpp.Element("presence", { to: "room2@muc.localhost/" + (nconf.get("account:jid")).split("@")[0] }).
-        c('x', { xmlns: 'http://jabber.org/protocol/muc' }));
-    //console.log((nconf.get("account:jid")).split("@")[0]);
-    
-    var iq = new xmpp.Element('iq', { to: "room2@muc.localhost", type: 'set' }).
-            c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'}).
-            c("x", { xmlns:"jabber:x:data", type:"submit" }).
-            c("field", { "var":'muc#roomconfig_roomname' }).
-            c("value").
-            t("Room2");
-
-    conn.send(iq);
-
-    setInterval(function() {
-        conn.send(" ");
-    }, 30000);
-});
-*/
 
 function create_room(room_jid, room_name, cb){
     // Establish a connection
@@ -56,6 +33,7 @@ function create_room(room_jid, room_name, cb){
         conn.send(room_presence);
         // Set room's name
         conn.send(room_iq);
+        //conn.end();
         
         cb();
     });
@@ -68,9 +46,20 @@ app.configure(function(){
 });
 
 app.get("/", function(reqP, resP){
-    create_room("room3", "Room3", function(){
-        resP.json(200, "OK");
+	cons.swig("./main.html",{},function(err,html){
+		resP.writeHead(200, {"Content-Type" : "text/html"});
+		resP.end(html);
+	});
+});
+
+app.post("/", function(reqP, resP){
+    var room_jid = reqP.body.room_jid;
+    var room_name = reqP.body.room_name;
+    
+    console.log("PARAMS: "+room_jid+"   "+room_name);
+    create_room(room_jid, room_name, function(){
+        resP.json(200, {"message" : "OK"});
     });
 });
 
-app.listen(nconf.get("app:port"), nconf.get("app:host"));
+app.listen(nconf.get("app:port"), nconf.get("app:host"));var nconf = require("nconf");
