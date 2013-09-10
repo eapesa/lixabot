cdec4bb883c5e0b293a2d305ee009d74b1943ee0
Elixa-Mae Pesa <eapesa@gmail.com>
2013-09-10 08:51:40 +0800

    can now destroy a room

--- app.js
+++ app.js
@@ -26,16 +26,46 @@ function create_room(room_jid, room_name, cb){
                 c("value").
                 t(room_name);
         
-        console.log("We're online!");
+        console.log("Creating room...");
         // Set client's presence
         conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
         // Send new room's presence
         conn.send(room_presence);
         // Set room's name
         conn.send(room_iq);
-        //conn.end();
         
         cb();
+        
+        conn.end();
+    });
+}
+
+function destroy_room(room_jid, cb){
+    console.log(room_jid);
+    
+    // Establish a connection
+    var conn = new xmpp.Client(nconf.get("account"));
+    
+    conn.on("online", function(){
+        var room_presence = new xmpp.Element("presence", { 
+            to: room_jid + "@muc.localhost/" + (nconf.get("account:jid")).split("@")[0] 
+        }).c('x', { xmlns: 'http://jabber.org/protocol/muc' });
+        
+        var room_iq = new xmpp.Element('iq', { to: room_jid + "@muc.localhost", type: 'set' }).
+                c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'}).
+                c("destroy");
+        
+        console.log("Destroying room...");
+        // Set client's presence
+        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
+        // Send new room's presence
+        conn.send(room_presence);
+        // Set room's name
+        conn.send(room_iq);
+        
+        cb();
+        
+        conn.end();
     });
 }
 
@@ -52,13 +82,28 @@ app.get("/", function(reqP, resP){
 	});
 });
 
-app.post("/", function(reqP, resP){
+app.post("/rooms", function(reqP, resP){
     var room_jid = reqP.body.room_jid;
     var room_name = reqP.body.room_name;
     
-    console.log("PARAMS: "+room_jid+"   "+room_name);
+    var reply = {
+        "status_code" : 200,
+        "message" : "OK"
+    };
     create_room(room_jid, room_name, function(){
-        resP.json(200, {"message" : "OK"});
+        resP.json(200, JSON.stringify(reply));
+    });
+});
+
+app.delete("/rooms/:room_jid", function(reqP, resP){
+    var room_jid = reqP.params.room_jid;
+    console.log("DELETING "+room_jid);
+    var reply = {
+        "status_code" : 200,
+        "message" : "OK"
+    };
+    destroy_room(room_jid, function(){
+        resP.json(200, JSON.stringify(reply));
     });
 });
 
