d9f1fb803ced75d109f19357ff87dbe148183336
Elixa-Mae Pesa <eapesa@gmail.com>
2013-09-10 10:40:51 +0800

    can now detect existing rooms

--- app.js
+++ app.js
@@ -1,73 +1,14 @@
-var xmpp = require("node-xmpp");
-var request = require("request");
 var nconf = require("nconf");
 var cons = require("consolidate");
 var express = require("express");
 
 var app = express();
 
+var user = require("./manage_rooms");
+
 nconf.use("file", {
     file: "./config.json"
 });
-
-function create_room(room_jid, room_name, cb){
-    // Establish a connection
-    var conn = new xmpp.Client(nconf.get("account"));
-    
-    conn.on("online", function(){
-        var room_presence = new xmpp.Element("presence", { 
-            to: room_jid + "@muc.localhost/" + (nconf.get("account:jid")).split("@")[0] 
-        }).c('x', { xmlns: 'http://jabber.org/protocol/muc' });
-        
-        var room_iq = new xmpp.Element('iq', { to: room_jid + "@muc.localhost", type: 'set' }).
-                c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'}).
-                c("x", { xmlns:"jabber:x:data", type:"submit" }).
-                c("field", { "var":'muc#roomconfig_roomname' }).
-                c("value").
-                t(room_name);
-        
-        console.log("Creating room...");
-        // Set client's presence
-        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
-        // Send new room's presence
-        conn.send(room_presence);
-        // Set room's name
-        conn.send(room_iq);
-        
-        cb();
-        
-        conn.end();
-    });
-}
-
-function destroy_room(room_jid, cb){
-    console.log(room_jid);
-    
-    // Establish a connection
-    var conn = new xmpp.Client(nconf.get("account"));
-    
-    conn.on("online", function(){
-        var room_presence = new xmpp.Element("presence", { 
-            to: room_jid + "@muc.localhost/" + (nconf.get("account:jid")).split("@")[0] 
-        }).c('x', { xmlns: 'http://jabber.org/protocol/muc' });
-        
-        var room_iq = new xmpp.Element('iq', { to: room_jid + "@muc.localhost", type: 'set' }).
-                c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'}).
-                c("destroy");
-        
-        console.log("Destroying room...");
-        // Set client's presence
-        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
-        // Send new room's presence
-        conn.send(room_presence);
-        // Set room's name
-        conn.send(room_iq);
-        
-        cb();
-        
-        conn.end();
-    });
-}
 
 app.configure(function(){
     app.use("/", express.static(__dirname));
@@ -76,7 +17,7 @@ app.configure(function(){
 });
 
 app.get("/", function(reqP, resP){
-	cons.swig("./main.html",{},function(err,html){
+	cons.swig("./media/main.html",{},function(err,html){
 		resP.writeHead(200, {"Content-Type" : "text/html"});
 		resP.end(html);
 	});
@@ -90,19 +31,28 @@ app.post("/rooms", function(reqP, resP){
         "status_code" : 200,
         "message" : "OK"
     };
-    create_room(room_jid, room_name, function(){
+    user.create_room(room_jid, room_name, function(){
         resP.json(200, JSON.stringify(reply));
     });
 });
 
 app.delete("/rooms/:room_jid", function(reqP, resP){
     var room_jid = reqP.params.room_jid;
-    console.log("DELETING "+room_jid);
     var reply = {
         "status_code" : 200,
         "message" : "OK"
     };
-    destroy_room(room_jid, function(){
+    user.delete_room(room_jid, function(){
+        resP.json(200, JSON.stringify(reply));
+    });
+});
+
+app.get("/rooms", function(reqP, resP){
+    var reply = {
+        "status_code" : 200,
+        "message" : "OK"
+    };
+    user.discover_rooms(function(){
         resP.json(200, JSON.stringify(reply));
     });
 });
---
+++ manage_rooms.js
@@ -0,0 +1,75 @@
+var xmpp = require("node-xmpp");
+var request = require("request");
+var nconf = require("nconf");
+
+nconf.use("file", {
+    file: "./config.json"
+});
+
+function go_online(conn, cb){
+    conn.on("online", function(){
+        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat")); 
+        cb();
+    });
+}
+
+exports.create_room = function(room_jid, room_name, cb){
+    var conn = new xmpp.Client(nconf.get("account"));
+    
+    go_online(conn, function(){
+        var room_presence = new xmpp.Element("presence", { 
+            to: room_jid + "@muc.localhost/" + (nconf.get("account:jid")).split("@")[0] 
+        }).c('x', { xmlns: 'http://jabber.org/protocol/muc' });
+        
+        var room_iq = new xmpp.Element('iq', { to: room_jid + "@muc.localhost", type: 'set' })
+                .c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'})
+                .c("x", { xmlns:"jabber:x:data", type:"submit" })
+                .c("field", { "var":'muc#roomconfig_roomname' })
+                .c("value")
+                .t(room_name);
+        
+        console.log("Creating room...");
+        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat")); 
+        // Send new room's presence
+        conn.send(room_presence);
+        // Set room's name
+        conn.send(room_iq);
+        
+        cb();
+        conn.end();
+    });
+}
+
+exports.delete_room = function(room_jid, cb){
+    var conn = new xmpp.Client(nconf.get("account"));
+    
+    go_online(conn, function(){    
+        var room_iq = new xmpp.Element('iq', { to: room_jid + "@muc.localhost", type: 'set' })
+                .c("query", { xmlns : 'http://jabber.org/protocol/muc#owner'})
+                .c("destroy");
+    
+        console.log("Destroying room...");
+        conn.send(new xmpp.Element("presence", { type: "available" }).c("show").t("chat"));
+        conn.send(room_iq);
+        
+        cb();
+        conn.end();
+    });
+}
+
+exports.discover_rooms = function(cb){
+    var conn = new xmpp.Client(nconf.get("account"));
+    go_online(conn, function(){
+        var room_iq = new xmpp.Element('iq', { to: "muc.localhost", type: 'get' })
+                .c("query", { xmlns : 'http://jabber.org/protocol/disco#items'});
+    
+        console.log("Discovering rooms...");
+        conn.send(room_iq);
+        
+        conn.on("stanza", function(stanza){
+            console.log(stanza.toString());
+            cb();
+            conn.end();
+        });
+    });
+}
